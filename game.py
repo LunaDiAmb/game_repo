4c0d31ab7194b1a1ec62777422cc4a843669dc07
LunaDiAmb <ddiambrosio7467@floridapoly.edu>
2025-11-12 21:28:26 +0000

    Person 1 finishes

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
-print("welcome")
+print("Welcome to rock-paper-scissors")
+
+player_choice = str(input("Choose rock, paper, or scissors: "))
+
+print("You chose:" , player_choice)
+
+# --------------- you do computer choice ---------------
+
+if player_choice.lower().strip() == computer_choice.lower().strip():
+    print("You win")
+else:
+    print("You lose")
